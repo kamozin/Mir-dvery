c796d40b810ed5793b162fddcd4cb9658223657c
kamozin <kamozin@example.ru>
2016-07-05 17:55:27 +0300

    Справочники старт

---
+++ app/Http/Controllers/Admin/DirectoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use App\Actions;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+
+use Validator;
+use Storage;
+class DirectoryController extends Controller
+{
+
+
+
+public function  store(){return view('admin.directory.store');}
+
+
+
+
+
+}
